9c6fd9f206a42138d8c478f6edcd81a540e5cc4f
Konstantine <Konstaphy@gmail.com>
2023-04-08 19:15:28 +0500

    связан фронт с с3

--- src/main.py
+++ src/main.py
@@ -8,10 +8,10 @@ from src.views.video.video import video_view
 
 if __name__ == "__main__":
     app = Flask(__name__)
+    CORS(app, supports_credentials=True)
     app.register_blueprint(users_view, url_prefix="/users")
     app.register_blueprint(video_view, url_prefix="/videos")
     # initializing db cursor
     db = db.cursor()
-    CORS(app, supports_credentials=True)
     # run app in 8080
     app.run(port=8080)
--- src/views/video/video.py
+++ src/views/video/video.py
@@ -1,16 +1,20 @@
 from flask import Blueprint, jsonify, request
+from flask_cors import cross_origin
+
 from src.database.s3_client import s3client
 
-video_view = Blueprint('video', __name__)
+video_view = Blueprint('videos', __name__, url_prefix="/videos")
 
 
-@video_view.route('/upload')
+@video_view.route('/upload', methods=["POST"])
+@cross_origin(supports_credentials=True)
 def upload():
+    print(request.files)
     # Get the uploaded file data from the request object
-    file = request.files['file']
+    file = request.files['video']
 
     # Upload the file to S3
-    s3client.upload_fileobj(file, 'tuuub', 'videos')
+    s3client.upload_fileobj(file, 'tuuub', 'videos/' + file.filename)
 
     return 'File uploaded successfully!'
 
